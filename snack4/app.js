6231a55e43039d6f1b1e2a72ae38d17f8d910728
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-02-09 16:52:49 +0100

    complete snack 4

--- snack4/app.js
+++ snack4/app.js
@@ -3,3 +3,11 @@ Crea un generatore di funzioni creaTimer
 */
 
 //Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+
+function creaTimer(time){
+  return setTimeout(() => {
+    console.log("Tempo scaduto!")
+  }, time);
+}
+
+creaTimer(2000);
