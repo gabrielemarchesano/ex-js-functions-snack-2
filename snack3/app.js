13734795e09adb82b4869a741bd68eca507425c0
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-02-09 14:19:53 +0100

    complete snack 3

--- snack3/app.js
+++ snack3/app.js
@@ -3,3 +3,14 @@ Crea una funzione eseguiOperazione
 */
 
 // Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
+function somma(num1, num2){
+  return num1 + num2;
+}
+
+const sottrazione = (num1, num2) => num1 - num2;
+
+const eseguiOperazione = (num1, num2, operatore) => operatore(num1, num2);
+
+console.log(eseguiOperazione(1, 9, somma));
+console.log(eseguiOperazione(1, 9, sottrazione));
