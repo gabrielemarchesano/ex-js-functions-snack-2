df8da52eaa0ac5189d79ba9ea6d246a7c8cb99b5
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-02-09 18:41:52 +0100

    complete snack 7

--- snack7/app.js
+++ snack7/app.js
@@ -3,3 +3,20 @@ Crea una funzione che ferma un timer dopo un certo tempo
 */
 
 //Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+
+const eseguiEferma = (messaggio, start, stop) => {  
+
+  setTimeout(() => {
+    const stampaMessaggio = setInterval(() => {
+        console.log(messaggio);
+      }, 1000);
+
+    setTimeout(() => {
+      clearInterval(stampaMessaggio)
+    }, stop);
+  }, start);
+
+}
+
+
+eseguiEferma("ciao", 3000, 10000);
