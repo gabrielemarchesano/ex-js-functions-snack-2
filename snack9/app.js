29f39877a661aadc8a5f1fd2ee928144c7972863
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-02-11 18:11:17 +0100

    complete snack 9

---
+++ snack9/app.js
@@ -0,0 +1,21 @@
+/* Snack 8
+Creare una funzione che esegue una sequenza di operazioni con ritardi
+*/
+
+// Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+
+// Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.
+
+function sequenzaOperazioni(array, intervallo){
+  for(let i = 0; i < array.length; i++){
+    setTimeout(() => {
+      array[i]();
+      }, intervallo * i);
+    }
+}
+
+sequenzaOperazioni([
+  () => console.log("Operazione 1"),
+  () => console.log("Operazione 2"),
+  () => console.log("Operazione 3")
+], 2000);
